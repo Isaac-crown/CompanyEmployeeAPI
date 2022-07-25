cab706444498f9b82af9944351763de283efbada
mannypulator <emeritus93@gmail.com>
2022-07-25 09:43:33 +0100

    Implemented Repository layer

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
 
 var app = builder.Build();
 
